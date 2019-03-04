1a59e1c543d3670c6be974fe550a8cf9f73e3a03
BIT Student <classroom@bgit.rs>
2019-03-04 14:13:18 +0100

    Bom

---
+++ BOM/browserObject.js
@@ -0,0 +1,86 @@
+/*var word2 = "Good morning!";
+console.log(word2);
+
+
+function infos() {
+    console.log(window.navigator.platform);
+    console.log(window.navigator.appVersion);
+    console.log(window.navigator.userAgent);
+}
+
+function isOnline() {
+    if (window.navigator.onLine) {
+        console.log("online");
+    }
+    else {
+        console.log("offline");
+    }
+}
+infos();
+isOnline();
+
+
+function screeN() {
+    console.log(window.screen.height, window.screen.width);
+    console.log(window.screen.availHeight);
+}
+
+screeN();
+function urlInformation() {
+
+    console.log("Href: " + window.location.href);
+    console.log("hostname: " + window.location.hostname);
+    console.log("protocol: " + window.location.protocol);
+    console.log("Search: " + window.location.search);
+}
+
+urlInformation();
+
+function reloadPage() {
+    window.location.reload;
+}
+
+reloadPage();
+
+
+function storeData(param, val) {
+    window.localStorage.setItem(param, val);
+}
+storeData("bka", "vhgh");
+
+function printData() {
+
+    if (window.sessionStorage.length) {
+        console.log(window.sessionStorageStorage);   /*promenili smo local u sessionStorage*/
+/*  else {
+}
+console.log("There is no data!");
+}
+}
+
+function removeData(string) {
+window.localStorage.removeItem(string);
+}
+removeData("bka");
+
+
+
+function greeting() {
+    alert("zdravo prijatelji!")
+}
+
+greeting();
+
+function choise() {
+    var userChoise = confirm("Da li ste sigurni?");
+
+}
+
+choise();
+*/
+
+function question() {
+    var someQuestion = prompt("Kako ste danas?");
+}
+
+question();
---
+++ BOM/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+function urlInformation() {
+ 
+    console.log(window.location.href);
+    console.log(window.location.hostname);
+    console.log(window.location.protocol);
+    console.log(window.location.search);
+}
+urlInformation();
