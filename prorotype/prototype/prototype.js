5e040552fffabd1255682ce8d2377970a4134933
BIT Student <classroom@bgit.rs>
2019-03-19 17:39:16 +0100

    proto

--- prorotype/prototype/prototype.js
+++ prorotype/prototype/prototype.js
@@ -1,81 +1,78 @@
-function Person(name, surname) {
-    this.name = name;
-    this.surname = surname;
-};
+class Person {
+    constructor(name, surname) {
+        this.surname = surname;
+        this.name = name;
+    }
 
-Person.prototype.getFullName = function () {
-    return this.name + " " + this.surname;
-};
-
-
-
-function Employee(name, surname, job, salary) {
-    this.job = job;
-    this.salary = salary;
-
-    Person.call(this, name, surname)
-};
-
-
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
-
-
-Employee.prototype.getData = function () {
-    return this.name + " " + this.surname + " " + this.salary
-};
-
-Employee.prototype.getSalary = function () {
-    console.log(this.salary);
-};
-
-Employee.prototype.increaseSalary = function () {
-    return this.salary + (this.salary * 10 / 100);
+    getFullName() {
+        return this.name + " " + this.surname;
+    }
 }
 
 
 
-function Developer(name, surname, job, salary, specialization) {
-    this.specialization = specialization;
-
-    Employee.call(this, name, surname, job, salary);
-};
-
-Developer.prototype = Object.create(Employee.prototype);
-Developer.prototype.constructor = Developer;
-
-Developer.prototype.getSpecialization = function () {
-    console.log(this.specialization);
-};
 
 
-
-function Manager(name, surname, job, salary, department) {
-    this.department = department
-
-    Employee.call(this, name, surname, job, salary);
-};
-
-Manager.prototype = Object.create(Employee.prototype);
-Manager.prototype.constructor = Manager;
-
-Manager.prototype.getDepartment = function () {
-    console.log(this.department);
-};
-
-Manager.prototype.changeDepartment = function (department) {
-    this.department = department;
+class Employee extends Person {
+    constructor(name, surname, job, salary) {
+        super(name, surname)
+        this.job = job;
+        this.salary = salary;
+    }
+    getData() {
+        return this.name + " " + this.surname + " " + this.salary
+    }
+    getSalary() {
+        console.log(this.salary);
+    }
+    increaseSalary() {
+        return this.salary + (this.salary * 10 / 100);
+    }
 };
 
 
 
-var Mika = new Person("Mika", "Mikic");
+
+
+
+
+
+class Developer extends Employee {
+    constructor(name, surname, job, salary, specialization) {
+        super(name, surname, job, salary);
+        this.specialization = specialization;
+    };
+    getSpecialization() {
+        console.log(this.specialization);
+    }
+}
+
+
+
+class Manager extends Employee {
+    constructor(name, surname, job, salary, department) {
+        super(name, surname, job, salary);
+        this.department = department
+
+    };
+
+    getDepartment() {
+        console.log(this.department);
+    };
+    changeDepartment(department) {
+        this.department = department;
+    };
+}
+
+
+
+const Mika = new Person("Mika", "Mikic");
 console.log(Mika.getFullName());
 
-var Employee1 = new Employee("Pera", "Peric", "Programer", 1500);
+const Employee1 = new Employee("Pera", "Peric", "Programer", 1500);
 console.log(Employee1.getFullName());
 console.log(Employee1.getData())
 console.log(Employee1.increaseSalary());
 
-var developer1 = new Developer("Marko", "Dragic", "Programer", 1500, "Information service");
+const developer1 = new Developer("Marko", "Dragic", "Programer", 1500, "Information service");
 console.log(developer1.getSpecialization());
